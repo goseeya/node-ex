2b64556387c1ed008bb1141c286b7b8d12e8adb0
goseeya <149542@gmail.com>
2022-11-07 17:47:58 +0100

    add post people

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -12,8 +12,16 @@ app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people})
 })
 
+app.post('api/people')
+
 app.post('./login',(req,res)=>{
-    res.send('POST')
+    // console.log(req.body);
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+
+    res.status(401).send('please provide credentials')
 })
 
 
