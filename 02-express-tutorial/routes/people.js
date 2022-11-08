8a5892247c9fad9d471831a56cc7cf1579fc4345
goseeya <149542@gmail.com>
2022-11-08 07:33:41 +0100

    refactor routes

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -11,10 +11,14 @@ const {
 } = require('../controllers/people')
 
 // we already have app people in appp.js
-router.get('/', getPeople)
-router.post('/', createPerson)
-router.post('/postman', createPersonPostman)
-router.put('/:id', updatePerson)
-router.delete('/:id', deletePerson)
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.post('/postman', createPersonPostman)
+// router.put('/:id', updatePerson)
+// router.delete('/:id', deletePerson)
+
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = router
