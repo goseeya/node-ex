11b1faeb0e0fbf0c15b8bcea77a95ba5bc8d514b
goseeya <149542@gmail.com>
2022-11-08 07:13:40 +0100

    add routes

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,30 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const router = express.Router();
 
-// let {people} = require('./data')
+let { people} = require('../data');
 
-const people = require('./routes/people')
 
-//static assets
-app.use(express.static('./methods-public'))
-//parse form data
-app.use(express.urlencoded({extended: false}))
-//parse json
-app.use(express.json()) // we handle json that we got
 
-app.use('api/people',)
-
-app.post('/login',(req,res)=>{
-    // console.log(req.body);
-    const {name} = req.body;
-    if(name){
-        return res.status(200).send(`Welcome ${name}`)
-    }
-
-    res.status(401).send('please provide credentials')
-})
-
-app.get('/api/people',(req,res)=>{
+router.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people})
 })
 
@@ -75,3 +56,5 @@ app.delete('/api/peope/:id',(req,res)=>{
 app.listen(500,()=>{
     console.log('Server is listening on port 5000...');
 })
+
+module.exports = router
